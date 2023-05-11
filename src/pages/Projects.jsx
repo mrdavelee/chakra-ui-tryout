e727c69475009a6ffd5fdbbd8eb55096b3d252d7
mrdavelee <mrdavelee2012@gmail.com>
2023-05-11 23:26:11 +0100

    projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -34,56 +34,74 @@ export const Projects = () => {
                 mainImg={require ('../assets/portImgs/building.png') } 
                 url='https://www.building.co.uk/' 
                 tags={['css', 'html', 'JavaScript', 'Jquery']}
-                description='To be completed'
+                description='Building was one of the first sites to go live on the new
+                Webvision Cloud SAAS service. Having tested the service entirely with our own 
+                branding it was refreshing to note the difference a new brand could make, as 
+                well as the comparitive speed at which we could deploy a new site.'
             />
             <Overlay 
                 title='Retail Week' 
                 mainImg={require ('../assets/portImgs/rw.png') } 
                 url='https://www.retail-week.com/' 
                 tags={['css', 'html', 'JavaScript', 'Jquery']}
-                description='To be completed'
+                description='Retail Week was the first Webvision Pro client, which involved
+                some changes to the underlying site structure. The site received high traffic 
+                after a BBC News article directed the public to an article.'
             />
             <Overlay 
                 title='Boat International' 
                 mainImg={require ('../assets/portImgs/boat.png') } 
                 url='https://www.boatinternational.com/' 
                 tags={['css', 'html', 'JavaScript', 'Jquery']}
-                description='To be completed'
+                description='Boat International requested some changes to be made to the structure
+                of their site, without using the pro option. I used CSS to manipulate the header
+                to accomodate their request, which turned out very well.'
             />
             <Overlay 
-                title='High Street Journal' 
+                title='Health Service Journal' 
                 mainImg={require ('../assets/portImgs/hsj.png') } 
                 url='https://www.hsj.co.uk/' 
                 tags={['css', 'html', 'JavaScript', 'Jquery']}
-                description='To be completed'
+                description='HSJ is another high-profile client in their field.'
             />
             <Overlay 
                 title='International Broadcasting Convention' 
                 mainImg={require ('../assets/portImgs/ibc.png') } 
                 url='https://www.ibc.org/' 
                 tags={['css', 'html', 'JavaScript', 'Jquery']}
-                description='To be completed'
+                description='IBC had dictated that the site must be finished before an event
+                they were hosting. We had the site finished well before the deadline, fully tested
+                and live. The front-end build inspired an update to the Webvision Cloud suite in the
+                form of new block elements to display, such as a video gallery.'
             />
             <Overlay 
                 title='The Law Gazette' 
                 mainImg={require ('../assets/portImgs/law.png') } 
                 url='https://www.lawgazette.co.uk/' 
                 tags={['css', 'html', 'JavaScript', 'Jquery']}
-                description='To be completed'
+                description='The Law Gazette was another client who requested a pro upgrade. 
+                Thre was a lot of alteration to the header and footer, which required some CSS
+                wrangling.'
             />
             <Overlay 
                 title='Education in Chemistry' 
                 mainImg={require ('../assets/portImgs/eic.png') } 
                 url='https://edu.rsc.org/eic' 
                 tags={['css', 'html', 'JavaScript', 'Jquery']}
-                description='To be completed'
+                description='EIC, as part of the Royal Society of Chemistry, requested subtle 
+                changes to a numnber of elements where news articles overlapped, or were shared.
+                This was done using classes applied using JavaScript which could then be styled
+                accordingly.'
             />
             <Overlay 
                 title='Chemistry World' 
                 mainImg={require ('../assets/portImgs/cw.png') } 
                 url='https://www.chemistryworld.com/' 
                 tags={['css', 'html', 'JavaScript', 'Jquery']}
-                description='To be completed'
+                description='Chemistry World was intended to be a more layperson-friendly news 
+                source for chemistry. with the design changes they requested a pro subscription 
+                was necessary. the header needed to be reconfigured heavily from the standard 
+                version and the footer area needed a rebuild as well as refactored carousels.'
             />
         </SimpleGrid>
         

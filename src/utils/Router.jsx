682f8017d8c17fe6a324daf5d48073be93de8138
David Lee <mrdavelee2012@gmail.com>
2024-10-24 21:21:41 +0100

    Remove fat fighters

--- src/utils/Router.jsx
+++ src/utils/Router.jsx
@@ -3,13 +3,13 @@ import { createBrowserRouter } from "react-router-dom"
 import { Work } from "../pages/Work"
 import { Home } from "../pages/Home"
 import { Projects } from "../pages/Projects"
-import Register from "../fat-fighters/Register"
+// import Register from "../fat-fighters/Register"
 
 export const router = createBrowserRouter([
     { path: "/", element: <Home /> },
     { path: "/my-work", element: <Work /> },
     { path: "/my-projects", element: <Projects /> },
     // { path: "/fat-fighters", element: <Fat /> },
-    { path: "/fat-fighters", element: <Register /> },
+    // { path: "/fat-fighters", element: <Register /> },
   ])
   
